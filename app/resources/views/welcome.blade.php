9e2d33b00f84887d635df56c079a0f5eefa97154
Thinus Barnard | Front End Engineer <thinus.barnard@genexinstitute.com>
2023-02-02 10:12:14 +0200

    Fix for windows environments

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -17,28 +17,28 @@
     </head>
     <body class="antialiased">
         <hr>
-        <a href="http://0.0.0.0/" target="_blank">Home Page</a> &nbsp;
-        <a href="http://0.0.0.0:8081/" target="_blank">PHPMyAdmin</a> &nbsp;
-        <a href="http://0.0.0.0:8082/" target="_blank">Redis Commander</a> &nbsp;
-        <a href="http://0.0.0.0:8083/" target="_blank">RabbitMQ</a> &nbsp;
-        <a href="http://0.0.0.0:8084/" target="_blank">MailHog</a> &nbsp;
+        <a href="http://localhost/" target="_blank">Home Page</a> &nbsp;
+        <a href="http://localhost:8081/" target="_blank">PHPMyAdmin</a> &nbsp;
+        <a href="http://localhost:8082/" target="_blank">Redis Commander</a> &nbsp;
+        <a href="http://localhost:8083/" target="_blank">RabbitMQ</a> &nbsp;
+        <a href="http://localhost:8084/" target="_blank">MailHog</a> &nbsp;
         <hr>
         <p>
-            Setup your db in <a href="http://0.0.0.0:8081/" target="_blank">PHPMyAdmin</a> then write some migrations.
+            Setup your db in <a href="http://localhost:8081/" target="_blank">PHPMyAdmin</a> then write some migrations.
         </p>
         <p>
             By visiting this home page, you have set a key in the
-            <a target="_blank" href="http://0.0.0.0:8082">redis commander</a> to prove that the cache works, go
+            <a target="_blank" href="http://localhost:8082">redis commander</a> to prove that the cache works, go
             and have a look.
         </p>
         <p>
-            We have also dispatched a welcome mail into the <a target="_blank" href="http://0.0.0.0:8083">rabbitMQ</a>
+            We have also dispatched a welcome mail into the <a target="_blank" href="http://localhost:8083">rabbitMQ</a>
             queue service.
         </p>
         <p>
             You may view this mail by bashing into the <strong>app container</strong> and running
             <i><strong>php artisan queue:work</strong></i>. This will fire off the mail into your
-            <a target="_blank" href="http://0.0.0.0:8084">mail hog</a> inbox
+            <a target="_blank" href="http://localhost:8084">mail hog</a> inbox
         </p>
     </body>
 </html>
